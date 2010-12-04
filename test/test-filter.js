5e58efc08f347796a277dcc4f8ffb42d2bb6df2c
Mathieu Lecarme <mathieu@garambrogne.net>
2010-12-04 14:41:25 +0100

    poc

---
+++ test/test-filter.js
@@ -0,0 +1,35 @@
+var conf = require('./conf').conf,
+	TwitterNode = require('twitter-node').TwitterNode,
+	sys = require('sys'),
+	Reader = require('../lib/twitter-reader').Reader;
+
+var twit = new TwitterNode(conf);
+
+twit.addListener('error', function(error) {
+  console.log(error.message);
+});
+
+
+twit.track('football');
+
+twit
+  .addListener('tweet', function(tweet) {
+    sys.puts("@" + tweet.user.screen_name + ": " + tweet.text);
+		var r  = new Reader(tweet.text, function() {
+			
+		});
+  })
+
+  .addListener('limit', function(limit) {
+    sys.puts("LIMIT: " + sys.inspect(limit));
+  })
+
+  .addListener('delete', function(del) {
+    sys.puts("DELETE: " + sys.inspect(del));
+  })
+
+  .addListener('end', function(resp) {
+    sys.puts("wave goodbye... " + resp.statusCode);
+  })
+
+  .stream();
