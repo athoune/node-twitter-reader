6543db4a6fb6c28eb5c2018d47e55226f0cb7cca
Mathieu Lecarme <mathieu@garambrogne.net>
2010-12-04 21:29:11 +0100

    event/listener

--- test/test-filter.js
+++ test/test-filter.js
@@ -1,35 +1,40 @@
 var conf = require('./conf').conf,
-	TwitterNode = require('twitter-node').TwitterNode,
-	sys = require('sys'),
-	Reader = require('../lib/twitter-reader').Reader;
+TwitterNode = require('twitter-node').TwitterNode,
+sys = require('sys'),
+colors = require('colors'),
+Reader = require('../lib/twitter-reader').Reader;
 
 var twit = new TwitterNode(conf);
 
 twit.addListener('error', function(error) {
-  console.log(error.message);
+	console.log(error.message);
 });
 
 
 twit.track('movie');
 
+var reader = new Reader();
+
+reader.addListener('url', function(url) {
+	sys.debug(url.red);
+});
+
 twit
-  .addListener('tweet', function(tweet) {
-    sys.puts("@" + tweet.user.screen_name + ": " + tweet.text);
-		var r  = new Reader(tweet.text, function() {
-			
-		});
-  })
+.addListener('tweet', function(tweet) {
+	sys.puts("@" + tweet.user.screen_name + ": " + tweet.text);
+	reader.feed(tweet.text);
+})
 
-  .addListener('limit', function(limit) {
-    sys.puts("LIMIT: " + sys.inspect(limit));
-  })
+.addListener('limit', function(limit) {
+	sys.puts("LIMIT: " + sys.inspect(limit));
+})
 
-  .addListener('delete', function(del) {
-    sys.puts("DELETE: " + sys.inspect(del));
-  })
+.addListener('delete', function(del) {
+	sys.puts("DELETE: " + sys.inspect(del));
+})
 
-  .addListener('end', function(resp) {
-    sys.puts("wave goodbye... " + resp.statusCode);
-  })
+.addListener('end', function(resp) {
+	sys.puts("wave goodbye... " + resp.statusCode);
+})
 
-  .stream();
+.stream();
