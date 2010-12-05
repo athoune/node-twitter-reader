15f0f7ee050b78f34add4a92d941434374b59e9c
Mathieu Lecarme <mathieu@garambrogne.net>
2010-12-05 22:45:06 +0100

    all languages

--- test/test-filter.js
+++ test/test-filter.js
@@ -1,8 +1,15 @@
 var conf = require('./conf').conf,
-TwitterNode = require('twitter-node').TwitterNode,
-sys = require('sys'),
-colors = require('colors'),
-Reader = require('../lib/twitter-reader').Reader;
+	TwitterNode = require('twitter-node').TwitterNode,
+	sys = require('sys'),
+	colors = require('colors'),
+	fs = require('fs'),
+	ngram = require('ngram'),
+	Reader = require('../lib/twitter-reader').Reader;
+
+var fp = new ngram.FingerPrint();
+fs.readdirSync('/Applications/LibreOffice.app/Contents/basis-link/share/fingerprint/').forEach(function(lm) {
+	fp.register(lm.split('.')[0], '/Applications/LibreOffice.app/Contents/basis-link/share/fingerprint/' + lm);
+});
 
 var twit = new TwitterNode(conf);
 
@@ -11,30 +18,35 @@ twit.addListener('error', function(error) {
 });
 
 
-twit.track('movie');
-
-var reader = new Reader();
-
-reader.addListener('url', function(url) {
-	sys.debug(url.red);
-});
+twit.track('football');
 
 twit
-.addListener('tweet', function(tweet) {
-	sys.puts("@" + tweet.user.screen_name + ": " + tweet.text);
-	reader.feed(tweet.text);
-})
-
-.addListener('limit', function(limit) {
-	sys.puts("LIMIT: " + sys.inspect(limit));
-})
-
-.addListener('delete', function(del) {
-	sys.puts("DELETE: " + sys.inspect(del));
-})
-
-.addListener('end', function(resp) {
-	sys.puts("wave goodbye... " + resp.statusCode);
-})
-
-.stream();
+	.addListener('tweet', function(tweet) {
+		//sys.puts("@" + tweet.user.screen_name + ": " + tweet.text);
+		var reader = new Reader();
+		var n = new ngram.Ngrams();
+		reader.addListener('url', function(url) {
+			sys.debug(url.red);
+		});
+		reader.addListener('word', function(word) {
+			n.feed(word);
+		});
+		reader.addListener('end', function() {
+			var language = fp.guess(n);
+			if(language != 'english' && language != 'scots') {
+				sys.debug(language.yellow + ' : ' + ("@" + tweet.user.screen_name).blue + ' ' + tweet.text);
+			}
+		});
+		reader.feed(tweet.text);
+		
+	})
+	.addListener('limit', function(limit) {
+		sys.puts("LIMIT: " + sys.inspect(limit));
+	})
+	.addListener('delete', function(del) {
+		sys.puts("DELETE: " + sys.inspect(del));
+	})
+	.addListener('end', function(resp) {
+		sys.puts("wave goodbye... " + resp.statusCode);
+	})
+	.stream();
